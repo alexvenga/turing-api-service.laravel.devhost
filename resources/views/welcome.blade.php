37bd842697efe821fa38335e039dbda2f8318d8b
alexvenga <bubnov.alexey@gmail.com>
2023-02-07 20:18:55 +0300

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <script src="https://cdn.tailwindcss.com"></script>
+
+    </head>
+    <body class="antialiased">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+
+            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 space-y-4 flex flex-col items-center justify-center">
+                <div class="text-4xl font-bold text-red-700">Turing machine</div>
+                <div>
+                    <a class="text-blue-700 hover:text-red-700 underline hover:no-underline" href="">Пример работы сервиса</a>
+                </div>
+            </div>
+
+        </div>
+    </body>
+</html>
